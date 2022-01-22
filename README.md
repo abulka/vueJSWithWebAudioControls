c4c03d6228082614dbaaa00f04873d21a91fe9f9
micbuffa <micbuffa@gmail.com>
2022-01-22 18:28:05 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# vueJSWithWebAudioControls
-Example of a vueJS compiled project with the webaudiocontrols library imported in the HelloWorld component.
+# webaudio
+See HelloWorld.vue. Notice that the webaudiocontrols sprite sheets needs to be located in the public folder, not in the src folder. The build system will resolve automatically img src=./ but not with webaudio-knob, webaudio-switch etc. See source code (public dir with a subfolder for knobs, switches imgs), and look at HelloWorld.vue. In the src code, . means the public folder.
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+Do it twice (the first time it will raise errors in the import of webaudiocontrols, ctrl-c then again npm run serve should be ok)
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <webaudio-knob
+      src="./assetsForWebAudioControls/knobs/greenish_sloped.png"
+      midilearn="true"
+      height="50"
+      width="50"
+      sprites="100"
+      min="-24"
+      max="12"
+      step="0.1"
+      value="0"
+      id="/BigMuff/Input"
+      style="height: 50px"
+    >
+    </webaudio-knob>
+    <webaudio-knob
+      src="./assetsForWebAudioControls/knobs/Jambalaya.png"
+      height="50"
+      width="50"
+      sprites="100"
+      min="0"
+      max="1"
+      step="0.01"
+      midilearn="true"
+      value="0.5"
+      id="/GuitarAmpSim60s/Bass"
+    >
+    </webaudio-knob>
+    <br />
+    <webaudio-switch
+      src="./assetsForWebAudioControls/switches/switch_1.png"
+      midilearn="true"
+      height="32"
+      width="55"
+      id="/BigMuff/bypass"
+    >
+    </webaudio-switch>
+  </div>
+</template>
+
+<script>
+import "../lib/webaudio-controls.js";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
