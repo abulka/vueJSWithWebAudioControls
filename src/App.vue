08a80878c92f88920ca327d2b6b6c41ae5494d22
Andy Bulka <abulka@gmail.com>
2022-01-25 11:53:47 +1100

    knob component adjusting global knob state

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <WebAudioKnobVue msg="boo" v-bind:val="knob1Value" @changeKnobValue="knob1Value = $event"></WebAudioKnobVue>
-    <!-- <WebAudioKnobVue msg="boo" :val.sync="knob1Value"  @changeKnobValue="knob1Value = $event"></WebAudioKnobVue> -->
-    <p>Parent knob1Value is:</p>
+    <h2>Parent knob1Value is:</h2>
     <h1>{{ knob1Value }}</h1>
     <button @click="knob1Value = 0">set to 0</button>
     <button @click="knob1Value = 20">set to 20</button>
     <button @click="knob1Value = 50">set to 50</button>
     <br>
+
+    <h2>knob component adjusting parent state</h2>
+    <WebAudioKnobVue msg="boo" v-bind:val="knob1Value" @changeKnobValue="knob1Value = $event"></WebAudioKnobVue>
+    <!-- <WebAudioKnobVue msg="boo" :val.sync="knob1Value"  @changeKnobValue="knob1Value = $event"></WebAudioKnobVue> -->
+    
+    <h2>knob component adjusting global knob state</h2>
+    <WebAudioKnobVue msg="boo" v-bind:val="$store.state.keyboardKeyGlobal" @changeKnobValue="$store.commit('setKeyboardKeyGlobal', $event)"></WebAudioKnobVue>
     
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
